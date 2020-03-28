d163497e0160b3cc66df3ac040defd07c94efc62
Kevin Mathias <komathias1@gmail.com>
2020-03-28 01:04:07 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+    while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  
+  array
+else print "Hey! Isn't this the same as the previous lab?!"
+end
 end
